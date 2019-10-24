ea16b49a4701d901b2996fb83b61804706d32e30
Forsoothatooth <56567039+Forsoothatooth@users.noreply.github.com>
2019-10-24 17:39:43 -0400

    Add files via upload

---
+++ adjustscale.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adjustscale : MonoBehaviour
+{
+    public float x, y, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = z = 1f;
+        y = 0.5f;
+    }
+    private void Update()
+    {
+        transform.localScale = new Vector3(x, y, z);
+    }
+
+}
