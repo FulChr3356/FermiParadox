a3e80315ad89f451dcb2de9dc8dd515a0707640f
FulChr3356 <51063994+FulChr3356@users.noreply.github.com>
2019-11-07 15:11:14 -0500

    dash added

--- Movement.cs
+++ Movement.cs
@@ -6,14 +6,23 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody2D rigid;
     private CharacterStats plStats;
-    //private GameObject chargeMeter;
     private float speed;
+    public float savedSpeed;
+    private bool overheated;
     private float chargeAmt;
     private bool isFlying;
     private bool spaced;
-    private bool overheated;
-    private int timer;
-    private float rise;
+    public int timer;
+    private float rise; 
+    private bool isDashing;
+    public float dashTimer =0;
+    public float maxDash = 0;
+    public Vector2 savedVelocity;
+    public DashState dashState;
+    public DashArrow arrowState;
+    public float dashX = 3;
+    public float dashY = 0; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,14 +33,14 @@ public class Movement : MonoBehaviour
         chargeAmt = plStats.chargeAmt;
         overheated = false;
         timer = 0;
-       // isFlying = false;
+        isFlying = false;
+        isDashing = false;
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-
         spaced = Input.GetKey(KeyCode.Space) || (Input.GetKey(KeyCode.Space) && Input.anyKey);
         if (!spaced || overheated)
         {
@@ -42,19 +51,19 @@ public class Movement : MonoBehaviour
             timer++;
         }
 
-            
-        if(spaced && !overheated &&chargeAmt >= 0)
+
+        if (spaced && !overheated && chargeAmt >= 0)
         {
             if (rise <= 5)
                 rise += 0.4f;
             timer = 0;
-            if(rigid.velocity.y <= 0)
-            rigid.velocity = new Vector2(Mathf.Lerp(0f, Input.GetAxis("Horizontal") * speed, 0.8f), Mathf.Lerp(rigid.velocity.y, rise, 0.8f));
+            if (rigid.velocity.y <= 0)
+                rigid.velocity = new Vector2(Mathf.Lerp(0f, Input.GetAxis("Horizontal") * speed, 0.8f), Mathf.Lerp(rigid.velocity.y, rise, 0.8f));
             else
                 rigid.velocity = new Vector2(Mathf.Lerp(0f, Input.GetAxis("Horizontal") * speed, 0.8f), Mathf.Lerp(0f, rise, 0.8f));
 
             chargeAmt--;
-            GetComponentInChildren<adjustscale>().x -= (1/ plStats.chargeAmt);
+            GetComponentInChildren<adjustscale>().x -= (1 / plStats.chargeAmt);
 
             if (spaced && chargeAmt <= 0)
             {
@@ -75,11 +84,11 @@ public class Movement : MonoBehaviour
         {
             chargeAmt += 2;
             this.GetComponentInChildren<adjustscale>().x += 2 / plStats.chargeAmt;
-           
+
         }
         else if (chargeAmt <= plStats.chargeAmt && timer >= 90)
         {
-            this.GetComponentInChildren<adjustscale>().x += 1/plStats.chargeAmt;
+            this.GetComponentInChildren<adjustscale>().x += 1 / plStats.chargeAmt;
             chargeAmt++;
         }
 
@@ -87,5 +96,92 @@ public class Movement : MonoBehaviour
 
         print(chargeAmt);
 
+
+
+
+
+
+        switch (dashState)
+        {
+            case DashState.Ready:
+                if (chargeAmt <= 40 || overheated)
+                    break;
+                var isDashKeyDown = Input.GetKeyDown(KeyCode.LeftShift);
+                if (isDashKeyDown)
+                {
+                    savedSpeed = speed;
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        arrowState = DashArrow.FowardPressed;
+                        dashX = 25;
+                        dashY = 0;
+                    }
+
+
+
+                     else if (Input.GetKey(KeyCode.UpArrow))
+                    {
+                        arrowState = DashArrow.UpPressed;
+                        dashY = 25;
+                        dashX = 0;
+                    }
+                   else  if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        arrowState = DashArrow.BackPressed;
+                        dashX = -25;
+                        dashY = 0;
+                    }
+
+                   else  if (Input.GetKey(KeyCode.DownArrow))
+                    {
+                        arrowState = DashArrow.DownPressed;
+                        dashX = 0;
+                        dashY = -25;
+                    }
+                    chargeAmt -= 40;
+                    dashState = DashState.Dashing;
+                }
+                break;
+            case DashState.Dashing:
+
+                rigid.velocity = new Vector2(dashX, dashY);
+
+                
+                dashTimer += .5f;
+                if (dashTimer >= 5f)
+                {
+                    
+                    speed = savedSpeed;
+                    rigid.velocity = new Vector2(0, 0);
+                    dashState = DashState.Cooldown;
+                }
+                break;
+            case DashState.Cooldown:
+                dashTimer -= .5f;
+                if (dashTimer <= 0)
+                {
+                    dashTimer = 0;
+                    dashState = DashState.Ready;
+                }
+                break;
+        }
     }
 }
+                
+
+
+
+   
+public enum DashState
+{
+    Ready,
+    Dashing,
+    Cooldown
+}
+
+public enum DashArrow
+{
+    UpPressed,DownPressed,BackPressed, FowardPressed
+}
+
+
